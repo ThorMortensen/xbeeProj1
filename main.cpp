542166a1911d04d2e76baebd3e45e09be248a372
thor <send.tilmig@gmail.com>
2017-10-09 15:03:40 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: thor
+ *
+ * Created on October 9, 2017, 2:19 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"hello world";
+    
+    
+    return 0;
+}
+
